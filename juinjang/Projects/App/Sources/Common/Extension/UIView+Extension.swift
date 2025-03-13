22ae29a1f14bb950cde514ab197c4a093b732fad
yujin <ydbwlsjki60@gmail.com>
2025-03-13 22:43:01 +0900

    [Feat] UIView 특정 모서리 둥글게 하는 메서드 구성

--- juinjang/Projects/App/Sources/Common/Extension/UIView+Extension.swift
+++ juinjang/Projects/App/Sources/Common/Extension/UIView+Extension.swift
@@ -24,6 +24,25 @@ extension UIView {
         // 뷰의 배경으로 그레디언트 레이어 추가
         self.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
+    func roundCorners(cornerRadius: CGFloat, corner: Corner) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = corner.cornerMasks
+    }
 }
 
 
+enum Corner {
+    case top
+    case bottom
+    case all
+    
+    var cornerMasks: CACornerMask {
+        switch self {
+        case .top: [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        case .bottom: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        case .all: [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        }
+    }
+}
