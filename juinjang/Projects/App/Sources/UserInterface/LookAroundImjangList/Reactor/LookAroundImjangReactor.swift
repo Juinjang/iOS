42e6dff1b7d31982240636ad142b13e38dba4bd8
yujin <ydbwlsjki60@gmail.com>
2025-03-19 21:24:13 +0900

    [Feat] 임장 둘러보기 collectionView UI에 필요한 SectionModelType, 관련 model 정의

--- juinjang/Projects/App/Sources/UserInterface/LookAroundImjangList/Reactor/LookAroundImjangReactor.swift
+++ juinjang/Projects/App/Sources/UserInterface/LookAroundImjangList/Reactor/LookAroundImjangReactor.swift
@@ -64,3 +64,91 @@ final class LookAroundImjangReactor: Reactor {
         return sectionOfLookAroundImjangData
     }
 }
+
+enum SectionOfLookAroundImjangData: SectionModelType {
+    typealias ITEM = Row
+    
+    case contentsSection(items: [Row])
+    case selectAreaSection(items: [Row])
+    case imjangCountSection(items: [Row])
+    case imjangListSection(header: String, items: [Row])
+    
+    enum Row {
+        case contentsSection(content: LookAroundContent)
+        case selectAreaSection(area: Area)
+        case imjangCountSection(imjangCount: Int)
+        case imjangListSection(lookAroundImjang: LookAroundImjangNote)
+    }
+    
+    var items: [Row] {
+        switch self {
+            case .contentsSection(let items): return items
+            case .selectAreaSection(let items): return items
+            case .imjangCountSection(let items): return items
+            case .imjangListSection(_, let items): return items
+        }
+    }
+    
+    init(original: SectionOfLookAroundImjangData, items: [Row]) {
+    switch original {
+    case .contentsSection( _):
+        self = .contentsSection(items: items)
+
+    case .selectAreaSection(_):
+        self = .selectAreaSection(items: items)
+      
+    case .imjangCountSection(_):
+        self = .imjangCountSection(items: items)
+    
+    case .imjangListSection(let header, _):
+        self = .imjangListSection(header: header, items: items)
+    }
+  }
+ }
+
+enum LookAroundContent: CaseIterable {
+    case pencilShop
+    case myNote
+    
+    var title: String {
+        switch self {
+        case .pencilShop: return "연필상점"
+        case .myNote: return "마이노트"
+        }
+    }
+    
+    var iconImage: UIImage {
+        switch self {
+        case .pencilShop: return UIImage.ImjangList.pencil
+        case .myNote: return UIImage.ImjangList.myNote
+        }
+    }
+}
+
+struct userProfile {
+    let userNickname: String
+    let profileImageUrl: String
+}
+
+
+struct Area {
+    let si: String?
+    let gu: String?
+    let dong: [String]?
+}
+
+enum PropertyType: String, CaseIterable {
+    case APARTMENT
+    case VILLA
+    case OFFICE_TEL
+    case DETACHED_HOUSE
+    
+    var image: UIImage {
+        switch self {
+        case .APARTMENT: return .ImjangList.apartment
+        case .VILLA: return .ImjangList.villa
+        case .OFFICE_TEL: return .ImjangList.officeTel
+        case .DETACHED_HOUSE: return .ImjangList.detachedHouse
+        }
+    }
+}
