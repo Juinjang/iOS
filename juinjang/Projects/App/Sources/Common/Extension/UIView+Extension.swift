5138f83a2226f2ab5b2c46e2598e3166403670cf
yujin <ydbwlsjki60@gmail.com>
2025-03-19 20:07:23 +0900

    [Feat] Corner 불필요한 케이스 제거

--- juinjang/Projects/App/Sources/Common/Extension/UIView+Extension.swift
+++ juinjang/Projects/App/Sources/Common/Extension/UIView+Extension.swift
@@ -35,14 +35,12 @@ extension UIView {
 
 enum Corner {
     case top
-    case topLeft
     case bottom
     case all
     
     var cornerMasks: CACornerMask {
         switch self {
         case .top: [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        case .topLeft: [.layerMinXMinYCorner]
         case .bottom: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         case .all: [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         }
