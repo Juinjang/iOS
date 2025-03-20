3a364fdc3230dd5d7330a02c61eb59fb2ab269d8
yuzzin <ydbwlsjki60@gmail.com>
2025-03-20 10:42:50 +0900

    [Feat] SelectAreaView 구성

---
+++ juinjang/Projects/App/Sources/UserInterface/LookAroundImjangList/SelectArea/View/SelectAreaView.swift
@@ -0,0 +1,21 @@
+//
+//  SelectAreaView.swift
+//  juinjang
+//
+//  Created by 조유진 on 3/20/25.
+//
+
+import UIKit
+
+final class SelectAreaView: BaseView {
+    
+    override func configureHierarchy() {
+    }
+    
+    override func configureLayout() {
+    }
+    
+    override func configureView() {
+        super.configureView()
+    }
+}
