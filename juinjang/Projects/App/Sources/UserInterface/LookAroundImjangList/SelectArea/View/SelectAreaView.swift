3ea1fcc06cb18e57d1188b8386dec3458ca6b6dc
yuzzin <ydbwlsjki60@gmail.com>
2025-03-20 10:44:08 +0900

    [Feat] navigationView UI 구성

--- juinjang/Projects/App/Sources/UserInterface/LookAroundImjangList/SelectArea/View/SelectAreaView.swift
+++ juinjang/Projects/App/Sources/UserInterface/LookAroundImjangList/SelectArea/View/SelectAreaView.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 final class SelectAreaView: BaseView {
+    let naviagtionView = DefaultNavigationView().then {
+        $0.title = "임장 지역 선택"
+        $0.leftItem = [.pop]
+    }
     
     override func configureHierarchy() {
+        addSubview(naviagtionView)
     }
     
     override func configureLayout() {
+        naviagtionView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(44)
+        }
     }
     
     override func configureView() {
         super.configureView()
+    }
+}
     }
 }
