fdb54024af91e327bfbd731d3ab2ce5cb0df020f
yujin <ydbwlsjki60@gmail.com>
2025-04-01 16:31:15 +0900

    [Feat] PaddingButton 추가

---
+++ juinjang/Projects/App/Sources/Components/CustomView/PaddingButton.swift
@@ -0,0 +1,28 @@
+//
+//  PaddingButton.swift
+//  juinjang
+//
+//  Created by 조유진 on 4/1/25.
+//
+
+import UIKit
+
+class PaddingButton: UIButton {
+    private var padding = UIEdgeInsets(top: 15.0, left: 0.0, bottom: 15.0, right: 0.0)
+
+    init(padding: UIEdgeInsets) {
+        super.init(frame: .zero)
+        self.padding = padding
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.width += padding.left + padding.right
+        contentSize.height += padding.top + padding.bottom
+        return CGSize(width: contentSize.width, height: contentSize.height)
+    }
+}
