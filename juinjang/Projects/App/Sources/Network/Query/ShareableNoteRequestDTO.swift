460453288aad98622091e0c57223545934eb8f4f
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-06-11 18:36:01 +0900

    [Chore] model 정리

--- juinjang/Projects/App/Sources/Network/Query/ShareableNoteRequestDTO.swift
+++ juinjang/Projects/App/Sources/Network/Query/ShareableNoteRequestDTO.swift
@@ -1,20 +1,20 @@
 //
-//  SharedNoteRequestDTO.swift
+//  ShareableNoteRequestDTO.swift
 //  juinjang
 //
-//  Created by KimDongWoo on 6/7/25.
+//  Created by KimDongWoo on 6/11/25.
 //
 
-struct ShareableNoteRequestDTO: Encodable {
-    let sort: String?              // 예: "POPULAR" 또는 "LATEST"
-    let propertyType: String?      // 예: "APARTMENT", "VILLA", "OFFICE_TEL", "DETACHED_HOUSE"
-    let priceType: String?         // 예: "SALE", "PULL_RENT", "MONTHLY_RENT", "MARKET_PRICE"
-    let keyword: String?           // 예: "검색어"
-    let pageable: Pageable         // 필수
-
-    struct Pageable: Encodable {
-        let page: Int              // 예: 0
-        let size: Int              // 예: 20
-        let sort: [String]         // 예: ["createdAt,desc"]
+struct ShareableNoteRequestDTO: Codable {
+    let sort: String?
+    let propertyType: String?
+    let priceType: String?
+    let keyword: String?
+    let pageable: Pageable
+    
+    struct Pageable: Codable {
+        let page: Int
+        let size: Int
+        let sort: [String]
     }
 }
