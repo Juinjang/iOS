13bfbaf0a2b5624533ddfd64be1d719226eb8aeb
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-07-03 23:50:10 +0900

    [Fix] OpenNewPage2VC monthlyRent nil값 처리

--- juinjang/Projects/App/Sources/Model/Encodable/PostDto.swift
+++ juinjang/Projects/App/Sources/Model/Encodable/PostDto.swift
@@ -12,7 +12,7 @@ struct PostDto: Encodable {
     let propertyType: Int // 매물유형(0 - 아파트, 1 - 빌라, 2 - 오피스텔, 3 - 단독주택)
     let priceType: Int    // 가격유형(0 - 매매, 1 - 전세, 2 - 월세, 3 - 실거래가
     let price: String   // 가격 - 월세의 경우에만 보증금
-    let monthlyRent: String
+    let monthlyRent: String?
     let address: String   // 주소
     let roadAddress: String
     let addressDetail: String   // 상세주소
