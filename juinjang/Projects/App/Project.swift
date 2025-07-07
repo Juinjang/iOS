a560b40729951c1d19271a2d03db44fc0f254a3e
yujin <ydbwlsjki60@gmail.com>
2025-07-08 01:00:52 +0900

    [Feat] dev 앱 버전 1.2.7로 업데이트

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -76,7 +76,7 @@ let project = Project(
                     "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon-dev",
                     "CFBundleDisplayName": "주인장-개발",
                     "SWIFT_ACTIVE_COMPILATION_CONDITIONS": "DEV",
-                    "MARKETING_VERSION": "1.2.1", // Version
+                    "MARKETING_VERSION": "1.2.7", // Version
                     "CURRENT_PROJECT_VERSION": "1" // Build
                 ],
                 configurations: [
