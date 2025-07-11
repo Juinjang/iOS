1ea50e15bcab261f0dfcb6ff79c874017b800ca6
yujin <ydbwlsjki60@gmail.com>
2025-07-12 00:00:55 +0900

    [Fix] 둘러보기 화면 Empty UI 갱신 오류 수정

--- juinjang/Projects/App/Sources/UserInterface/LookAround/View/LookAroundViewController.swift
+++ juinjang/Projects/App/Sources/UserInterface/LookAround/View/LookAroundViewController.swift
@@ -103,11 +103,7 @@ final class LookAroundViewController: BaseViewController, View {
             .compactMap { $0.sectionOfExploreNotes }
             .distinctUntilChanged()
             .map { [weak self] sections in
-                if let notes = sections[3].items as? [ExploreNoteModel] {
-                    print("notes count: \(notes.count)")
-                    self?.mainView.collectionView.collectionViewLayout = self?.mainView.createCollectionViewLayout(isListEmpty: notes.isEmpty) ?? UICollectionViewLayout()
-                }
-                print("notes count: \(sections[3].items.count)")
+                self?.mainView.collectionView.collectionViewLayout = self?.mainView.createCollectionViewLayout(isListEmpty: sections[3].items.isEmpty) ?? UICollectionViewLayout()
                 return sections
             }
             .bind(to: mainView.collectionView.rx.items(dataSource: dataSource))
