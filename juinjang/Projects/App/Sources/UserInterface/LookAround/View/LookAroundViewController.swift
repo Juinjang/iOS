9ad5f4b203373de5b807f33ab27057e03c6e124b
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-07-16 00:36:22 +0900

    [Fix] 노트 상세 -> 둘러보기 좋아요 이벤트 전달

--- juinjang/Projects/App/Sources/UserInterface/LookAround/View/LookAroundViewController.swift
+++ juinjang/Projects/App/Sources/UserInterface/LookAround/View/LookAroundViewController.swift
@@ -67,6 +67,7 @@ final class LookAroundViewController: BaseViewController, View {
     
     private let cellEventRelay = PublishRelay<LookAroundEventType>()
     private let moreButtonTapRelay = PublishRelay<Void>()
+    private let likeEventRelay = PublishRelay<Int>()
     
     init(reactor: LookAroundReactor) {
         super.init()
@@ -165,8 +166,17 @@ final class LookAroundViewController: BaseViewController, View {
             .compactMap { $0.tappedNote }
             .bind(with: self) { owner, noteInfo in
                 let (sharedNoteId, buildingName) = noteInfo
-                owner.showLookAroundDetailVC(sharedNoteId: sharedNoteId, buildingName: buildingName)
+                owner.showLookAroundDetailVC(
+                    sharedNoteId: sharedNoteId,
+                    buildingName: buildingName,
+                    likeEventRelay: owner.likeEventRelay
+                )
             }
+            .disposed(by: disposeBag)
+        
+        likeEventRelay
+            .map { Reactor.Action.heartButtonDidTap(sharedNoteId: $0) }
+            .bind(to: reactor.action)
             .disposed(by: disposeBag)
     }
     
@@ -181,7 +191,9 @@ final class LookAroundViewController: BaseViewController, View {
         }
     }
     
-    private func showLookAroundDetailVC(sharedNoteId: Int, buildingName: String) {
+    private func showLookAroundDetailVC(sharedNoteId: Int,
+                                        buildingName: String,
+                                        likeEventRelay: PublishRelay<Int>) {
         let lookAroundDetailVC = ImjangDetailViewController(
             reactor: ImjangDetailViewReactor(
                 dependency: .init(
@@ -189,7 +201,7 @@ final class LookAroundViewController: BaseViewController, View {
                     title: buildingName,
                     sharedNoteRepository: SharedNoteRepository(),
                     pencilShopRepository: PencilShopRepository(),
-                    likeEventRelay: nil
+                    likeEventRelay: likeEventRelay
                 )
             )
         )
