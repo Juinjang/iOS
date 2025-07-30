cc745d7ecda5b45767be3c349e9f81d6b04e23ce
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-07-30 18:02:04 +0900

    [Chore] Dev 버전 관리

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -76,8 +76,8 @@ let project = Project(
                     "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon-dev",
                     "CFBundleDisplayName": "주인장-개발",
                     "SWIFT_ACTIVE_COMPILATION_CONDITIONS": "DEV",
-                    "MARKETING_VERSION": "1.2.11", // Version
-                    "CURRENT_PROJECT_VERSION": "2025.07.27.2" // Build
+                    "MARKETING_VERSION": "1.2.12", // Version
+                    "CURRENT_PROJECT_VERSION": "2025.07.30.1" // Build
                 ],
                 configurations: [
                     .debug(name: .debug, xcconfig: debugConfig),
