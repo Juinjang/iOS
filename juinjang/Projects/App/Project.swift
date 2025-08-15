81f21b573c7fc1e0c144d71241bd720f48f233ec
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-08-16 01:00:34 +0900

    [Chore] 버전 관리

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -52,7 +52,7 @@ let project = Project(
                     "CFBundleDisplayName": "주인장",
                     "SWIFT_ACTIVE_COMPILATION_CONDITIONS": "PROD",
                     "MARKETING_VERSION": "2.0.0", // Version
-                    "CURRENT_PROJECT_VERSION": "2025.08.12.2" // Build
+                    "CURRENT_PROJECT_VERSION": "2025.08.16.2" // Build
                 ],
                 configurations: [
                     .debug(name: .debug, xcconfig: releaseConfig),
