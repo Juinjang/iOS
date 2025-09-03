c218c642f2802775596e1a355050fe004ca9a152
yujin <ydbwlsjki60@gmail.com>
2025-09-03 22:18:14 +0900

    [Feat] 빌드 넘버 수정

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -44,7 +44,7 @@ let project = Project(
             appType: .prod,
             appDependencies: appDependencies,
             appVersion: "2.0.3",
-            build: "2025.09.03.1"
+            build: "2025.09.03.2"
         ),
         
         // MARK: - DEV
@@ -52,7 +52,7 @@ let project = Project(
             appType: .dev,
             appDependencies: appDependencies,
             appVersion: "2.0.3",
-            build: "2025.09.03.1"
+            build: "2025.09.03.2"
         )
     ],
     additionalFiles: [
