62edf3f74d9ebe495d64382fb0a0c2d786d59251
yujin <ydbwlsjki60@gmail.com>
2025-09-03 21:08:43 +0900

    [Feat] 버전 2.0.3 업데이트

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -43,16 +43,16 @@ let project = Project(
         .makeAppTarget(
             appType: .prod,
             appDependencies: appDependencies,
-            appVersion: "2.0.2",
-            build: "2025.08.25.1"
+            appVersion: "2.0.3",
+            build: "2025.09.03.1"
         ),
         
         // MARK: - DEV
         .makeAppTarget(
             appType: .dev,
             appDependencies: appDependencies,
-            appVersion: "2.0.2",
-            build: "2025.08.25.1"
+            appVersion: "2.0.3",
+            build: "2025.09.03.1"
         )
     ],
     additionalFiles: [
