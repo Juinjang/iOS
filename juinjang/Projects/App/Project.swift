73b137e0be6e8844125ddee17c303f6332f9832a
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-09-09 19:06:25 +0900

    [chore] version 수정

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -43,16 +43,16 @@ let project = Project(
         .makeAppTarget(
             appType: .prod,
             appDependencies: appDependencies,
-            appVersion: "2.0.4",
-            build: "2025.09.09.1"
+            appVersion: "2.0.5",
+            build: "2025.09.09.2"
         ),
         
         // MARK: - DEV
         .makeAppTarget(
             appType: .dev,
             appDependencies: appDependencies,
-            appVersion: "2.0.4",
-            build: "2025.09.09.1"
+            appVersion: "2.0.5",
+            build: "2025.09.09.2"
         )
     ],
     additionalFiles: [
