fd3d8274c69a2c5446833a6fde41ff08151aa464
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2025-12-27 17:02:53 +0900

    [fix] 버전 변경

--- juinjang/Projects/App/Project.swift
+++ juinjang/Projects/App/Project.swift
@@ -44,7 +44,7 @@ let project = Project(
             appType: .prod,
             appDependencies: appDependencies,
             appVersion: "2.0.7",
-            build: "2025.12.23.1"
+            build: "2025.12.27.1"
         ),
         
         // MARK: - DEV
