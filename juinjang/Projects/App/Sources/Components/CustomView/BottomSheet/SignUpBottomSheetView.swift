ed6f89da0a065096b48fd8a20df6d1319c260ace
dongwoo <73861795+ornwoo96@users.noreply.github.com>
2026-01-10 17:57:48 +0900

    [chore] amplitude - 로그인 유도 바텀 시트 예 버튼 클릭 시

--- juinjang/Projects/App/Sources/Components/CustomView/BottomSheet/SignUpBottomSheetView.swift
+++ juinjang/Projects/App/Sources/Components/CustomView/BottomSheet/SignUpBottomSheetView.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Then
 import SnapKit
+import AmplitudeSwift
 
 final class SignUpBottomSheetView: UIViewController {
     private let containerView = UIView().then {
@@ -122,6 +123,7 @@ final class SignUpBottomSheetView: UIViewController {
     }
     
     @objc private func acceptButtonTapped(_ sender: UIButton) {
+        amplitude.track(event: BaseEvent(eventType: AmpliEventName.signup_button_clicked.rawValue))
         present(SignUpViewController(.present), animated: true)
     }
 }
