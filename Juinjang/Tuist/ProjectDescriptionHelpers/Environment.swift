cb14e6ad0f84a328f68ba9d93dda03315930263a
yujin <ydbwlsjki60@gmail.com>
2026-02-14 17:46:19 +0900

    feat: iOS 최소 타겟 17 업데이트

--- Juinjang/Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Juinjang/Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -2,7 +2,7 @@
 import ProjectDescription
 
 public enum Environment {
-    public static let deploymentTargets: DeploymentTargets = .iOS("16.0")
+    public static let deploymentTargets: DeploymentTargets = .iOS("17.0")
     public static let destinations: Destinations = [.iPhone]
     public static let organizationName = "com.juinjangteam.Juinjang"
     public static let appVersion = "2.0.1"
