647cb8d6ac7b782d378c93c98abd633bc67f33aa
yujin <ydbwlsjki60@gmail.com>
2026-04-20 00:13:33 +0900

    refactor: 디자인시스템 모듈 그룹화 개선

---
+++ Projects/DesignSystem/Sources/Enum/DSNavigationBarStyle.swift
@@ -0,0 +1,14 @@
+//
+//  DSNavigationBarStyle.swift
+//  DesignSystem
+//
+//  Created by 조유진 on 4/20/26.
+//
+
+// MARK: - DSNavigationBarStyle
+
+public enum DSNavigationBarStyle {
+    case `default`
+    case search
+    case center
+}
